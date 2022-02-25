9d2a4bfc36adf476971565d00604a6833691a8d4
Kapil Soni <78656206+kapil4457@users.noreply.github.com>
2022-02-25 11:12:34 +0530

    cloudinary bug fix

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,9 @@ app.use("/api/v1/", user);
 app.use("/api/v1/", order);
 app.use("/api/v1/", payment);
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
