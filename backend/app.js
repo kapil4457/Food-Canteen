192d5fac5f499fc5f3e186999ad688e82036dea6
Kapil Soni <78656206+kapil4457@users.noreply.github.com>
2022-02-25 11:26:31 +0530

    cloudinary error

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(fileUpload());
 
 const errorMiddleware = require("./middleware/error");
@@ -29,9 +33,6 @@ app.use("/api/v1/", product);
 app.use("/api/v1/", user);
 app.use("/api/v1/", order);
 app.use("/api/v1/", payment);
-
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
